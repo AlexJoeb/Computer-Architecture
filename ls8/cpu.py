b2c0599b07146378a14ee0be35bb633591308d3a
Alexander Besse <alexanderjoeb@hotmail.com>
2020-08-20 17:49:08 -0500

    STACK, CALL, RET completed.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+SP = 7
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[SP] = 0xF4 # Set the stack pointer (SP) to 244 -
         self.pc = 0 # Program Counter (PC), the index into memory of the currently-executing instruction
         
 
@@ -36,9 +39,11 @@ class CPU:
             with open(args[0]) as file:
                 for line in file:
                     try:
-                        line = int(line.split('#')[0].strip(), 2)
-                        self.ram_write(address, line)
-                        address += 1
+                        line = line.split('#', 1)[0].strip()
+                        if line:
+                            line = int(line, 2)
+                            self.ram_write(address, line)
+                            address += 1
                     except ValueError:
                         print("Error: Unknown value fed into RAM.")
                         return
@@ -84,7 +89,12 @@ class CPU:
             0b10000010: 'LDI',
             0b01000111: 'PRN',
             0b00000001: 'HLT',
-            0b10100010: 'MUL'
+            0b10100010: 'MUL',
+            0b10100000: 'ADD',
+            0b01000101: 'PUSH',
+            0b01000110: 'POP',
+            0b01010000: 'CALL',
+            0b00010001: 'RET'
         }
 
         while running:
@@ -110,4 +120,57 @@ class CPU:
                 reg_num_b = self.ram_read(self.pc + 2)
                 self.reg[reg_num_a] *= self.reg[reg_num_b]
                 self.pc += 3
+            elif ins == 'ADD':
+                # Preform MUL Action.
+                reg_num_a = self.ram_read(self.pc + 1)
+                reg_num_b = self.ram_read(self.pc + 2)
+                self.reg[reg_num_a] += self.reg[reg_num_b]
+                self.pc += 3
+            elif ins == 'PUSH':
+                # Push to the stack stored in RAM.
+
+                # Decrement the stack pointer.
+                self.reg[SP] -= 1
+                self.reg[SP] &= 0xff
+
+                # Get the number to be stored in the stack.
+                register_number = self.ram_read(self.pc + 1)
+                value = self.reg[register_number] 
+
+                # Store address to save at and then store it.
+                save_addr = self.reg[SP]
+                self.ram[save_addr] = value
+
+                self.pc += 2
+            elif ins == 'POP':
+                # Push to the stack stored in RAM.
+                
+                # Store address to pop and the value of the pop'd item.
+                addr = self.reg[SP]
+                value = self.ram[addr]
+
+                register_number = self.ram[self.pc + 1]
+                self.reg[register_number] = value
+
+                self.reg[SP] += 1
+                self.pc += 2
+            elif ins == 'CALL':
+                from_addr = self.pc + 2
+                
+                self.reg[SP] -= 1 # Decrement stack pointer by 1.
+                
+                to_addr = self.reg[SP]
+                
+                self.ram[to_addr] = from_addr
+                
+                register_number = self.ram[self.pc + 1]
+                subroute_addr = self.reg[register_number]
+                
+                self.pc = subroute_addr
+            elif ins == 'RET':
+                from_addr = self.reg[SP]
+                return_addr = self.ram[from_addr]
+                self.reg[SP] += 1
+                self.pc = return_addr
+                
             else: print(f'Unknown Instruction Code: {i}')
